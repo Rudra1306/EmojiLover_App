9a21e43062c224a64f1996461a90bb30e498f988
Rudra1306 <rudraparikh6@gmail.com>
2023-07-14 01:50:50 +1000

    Initial Commit

---
+++ EmojiLover_App/EmojiLover_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLover_AppApp.swift
+//  EmojiLover_App
+//
+//  Created by Rudra on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLover_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
